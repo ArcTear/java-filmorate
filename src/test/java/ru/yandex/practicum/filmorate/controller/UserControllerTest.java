0fbaa6fa138d92f15d597fe0737f24a492eb7a53
ArcTear <antonzhuravlev666@yandex.ru>
2023-03-12 17:33:57 +0300

    delete: id generation, unnecessary validation

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -16,25 +17,44 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    private static String validUser;
+    private static String invalidUser;
+
+    @BeforeAll
+    static void initializeUsers() {
+        validUser = "{\"id\":1,\"name\":\"User Name\",\"login\":\"UserLogin\",\"birthday\":\"2015-03-01\",\"email\":\"email@mail.ru\"}";
+        invalidUser = "{\"id\":null,\"name\":\"User Name\",\"login\":\"User Login\",\"birthday\":\"2024-03-01\",\"email\":\"emailmail.ru\"}";
+    }
 
     @SneakyThrows
     @Test
-    void testValidation() {
-        String validUser = "{\"id\":1,\"name\":\"User Name\",\"login\":\"UserLogin\",\"birthday\":\"2015-03-01\",\"email\":\"email@mail.ru\"}";
-        String inValidUser = "{\"id\":null,\"name\":\"User Name\",\"login\":\"User Login\",\"birthday\":\"2024-03-01\",\"email\":\"emailmail.ru\"}";
+    void createValidUser() {
         mockMvc.perform(post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
                         .content(validUser))
                 .andExpect(status().isOk());
+    }
 
+    @SneakyThrows
+    @Test
+    void getValidUser() {
+        mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(validUser))
+                .andExpect(status().isOk());
         mockMvc.perform(get("/users")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
+    }
 
+    @SneakyThrows
+    @Test
+    void createCInvalidUser() {
         mockMvc.perform(post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(inValidUser))
+                        .content(invalidUser))
                 .andExpect(status().isBadRequest());
     }
 }
